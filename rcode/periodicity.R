098a767b50595f215d93a61e317e0fd40a884b96
rilla <r.illa.pujagut@gmail.com>
2015-11-29 13:40:28 +0100

    some refactoring

--- rcode/periodicity.R
+++ rcode/periodicity.R
@@ -7,7 +7,7 @@ library(parallel)
 library(getopt)
 
 SOURCE.DIR <- "/home/rilla/nucleServ/rcode/sourceables"
-sourced <- c("helperfuns", "wig_funs", "get_genes")
+sourced <- c("helperfuns", "wig_funs", "get_genes", "periodicity_funs")
 for (x in sourced) {
     source(paste0(SOURCE.DIR, "/", x, ".R"))
 }
@@ -34,58 +34,6 @@ for (i in names(args)) {
 
 ## Some function definitions ##################################################
 
-findFirstAndLast <- function(id, start, end, strand, dyads, chr)
-{
-    in.gene <- dyads[dyads >= start & dyads <= end]
-    if (strand == "-") {
-        in.gene <- rev(in.gene)
-    }
-    if (length(in.gene)) {
-        data.frame(id=id,
-                   chrom=chr,
-                   strand=strand,
-                   first=in.gene[1],
-                   last=in.gene[length(in.gene)])
-    } else {
-        NULL
-    }
-}
-
-ecov <- function (x, period)
-    (1 + sin(pi/2 + 2*pi/period*x)) * 0.8^(abs(x)/period)
-
-# Create a coverage for a gene given p1.pos and last.pos and their periodicity
-coverageChr <- function(nuc.start, nuc.end, nuc.length, strand, L, period)
-{
-    revIt <- function(xs)
-        rev(xs) * (-1)
-
-    cov <- rep(0, L)
-
-    for (i in seq_along(nuc.start)) {
-        sper <- floor(period/2)
-        xs <- (-sper):(period*floor(nuc.length[i]/period) + sper)
-
-        if (strand[i] == "+") {
-            a <- xs
-            b <- revIt(xs)
-        } else if (strand[i] == "-") {
-            a <- revIt(xs)
-            b <- xs
-        }
-
-        x <- nuc.start[i] + a
-        y <- nuc.end[i] + b
-
-        cov[round(x)] <- cov[round(x)] + ecov(a, period)
-        cov[round(y)] <- cov[round(y)] + ecov(b, period)
-    }
-
-    cov
-}
-
-## Load inputs ################################################################
-
 message("loading genes")
 genes <- readGenome(params$genome,
                     cols=c("TXSTART",
@@ -101,50 +49,9 @@ calls.rd <- with(calls.df,
 
 ## Do it ######################################################################
 
-chroms <- unique(genes$TXCHROM)
 message("identifying first and last nucleosomes")
-genes.by.chr <- lapply(chroms,
-                       function (chr) subset(genes,
-                                             TXCHROM == chr))
-names(genes.by.chr) <- chroms
-dyads.by.chr <- lapply(ranges(calls.rd), dyadPos)
-
-chr.lens <- sapply(genes.by.chr,
-                   compose(max,
-                           partial(`[[`,
-                                   "TXEND")))
-
-used.cols <- c("GENEID", "TXSTART", "TXEND", "TXSTRAND")
-genes.nucs <- do.call(rbind, lapply(
-    chroms,
-    function(chr)
-        do.call(rbind,
-                myFilter(iterDf(genes.by.chr[[chr]][, used.cols],
-                                findFirstAndLast,
-                                dyads.by.chr[[chr]],
-                                chr),
-                         Negate(is.null)))
-))
-
-genes.nucs$nuc.length <- abs(genes.nucs$last - genes.nucs$first)
-
-# Compute Coverage for all genome
-message("calculating theoretic coverage")
-covPredAll <- xlapply(
-    chroms,
-    function (chr)
-        do.call(coverageChr,
-                c(unname(as.list(subset(genes.nucs,
-                                        subset=chrom == chr,
-                                        select=c("first",
-                                                 "last",
-                                                 "nuc.length",
-                                                 "strand")))),
-                  chr.lens[[chr]] + 500,
-                  params$period)),
-    mc.cores=params$mc.cores
-)
-names(covPredAll) <- chroms
+genes.nucs <- findGenesNucs(genes, calls.rd, params$mc.cores)
+covPredAll <- getPeriodCov(genes.nucs, params$period, params$mc.cores)
 
 ## Store output ###############################################################
 
@@ -152,4 +59,4 @@ message("writting bigWig output")
 splited <- lapply(covPredAll, splitAtZeros)
 writeBigWig(splited, params$output, params$genome)
 
-###############################################################################
+##############################################################################
---
+++ rcode/sourceables/periodicity_funs.R
@@ -0,0 +1,120 @@
+#!/usr/bin/env Rscript
+
+## Imports ####################################################################
+
+library(IRanges)
+library(parallel)
+
+SOURCE.DIR <- "/home/rilla/nucleServ/rcode/sourceables"
+source(paste(SOURCE.DIR,
+             "helperfuns.R",
+             sep="/"))
+
+###############################################################################
+
+findFirstAndLast <- function(id, start, end, strand, dyads, chr)
+{
+    in.gene <- dyads[dyads >= start & dyads <= end]
+    if (strand == "-") {
+        in.gene <- rev(in.gene)
+    }
+    if (length(in.gene)) {
+        data.frame(id     = id,
+                   chrom  = chr,
+                   strand = strand,
+                   start  = start,
+                   end    = end,
+                   first  = in.gene[1],
+                   last   = in.gene[length(in.gene)])
+    } else {
+        NULL
+    }
+}
+
+ecov <- function (x, period)
+    (1 + sin(pi/2 + 2*pi/period*x)) * 0.8^(abs(x)/period)
+
+# Create a coverage for a gene given p1.pos and last.pos and their periodicity
+coverageChr <- function(nuc.start, nuc.end, nuc.length, strand, L, period)
+{
+    revIt <- function(xs)
+        rev(xs) * (-1)
+
+    cov <- rep(0, L)
+
+    for (i in seq_along(nuc.start)) {
+        sper <- floor(period/2)
+        xs <- (-sper):(period*floor(nuc.length[i]/period) + sper)
+
+        if (strand[i] == "+") {
+            a <- xs
+            b <- revIt(xs)
+        } else if (strand[i] == "-") {
+            a <- revIt(xs)
+            b <- xs
+        }
+
+        x <- nuc.start[i] + a
+        y <- nuc.end[i] + b
+
+        cov[round(x)] <- cov[round(x)] + ecov(a, period)
+        cov[round(y)] <- cov[round(y)] + ecov(b, period)
+    }
+
+    cov
+}
+
+findGenesNucs <- function (genes, calls, mc.cores=1)
+{
+    chroms <- unique(genes$TXCHROM)
+    genes.by.chr <- lapply(chroms,
+                           function (chr) subset(genes,
+                                                 TXCHROM == chr))
+    names(genes.by.chr) <- chroms
+    dyads.by.chr <- lapply(ranges(calls), dyadPos)
+
+    used.cols <- c("GENEID", "TXSTART", "TXEND", "TXSTRAND")
+    genes.nucs <- do.call(rbind, xlapply(
+        chroms,
+        function(chr)
+            do.call(rbind,
+                    myFilter(iterDf(genes.by.chr[[chr]][, used.cols],
+                                    findFirstAndLast,
+                                    dyads.by.chr[[chr]],
+                                    chr),
+                             Negate(is.null))),
+        mc.cores=mc.cores
+    ))
+    genes.nucs$nuc.length <- abs(genes.nucs$last - genes.nucs$first)
+
+    genes.nucs
+}
+
+getPeriodCov <- function (genes.nucs, period, mc.cores=1)
+{
+    chroms <- unique(genes.nucs$chrom)
+
+    chr.lens <- sapply(
+        chroms,
+        function(chr)
+            max(genes.nucs[genes.nucs$chrom == chr, "end"]) + 500
+    )
+    names(chr.lens) <- chroms
+
+    cov <- xlapply(
+        chroms,
+        function (chr)
+            do.call(coverageChr,
+                    c(unname(as.list(subset(genes.nucs,
+                                            subset=chrom == chr,
+                                            select=c("first",
+                                                     "last",
+                                                     "nuc.length",
+                                                     "strand")))),
+                      chr.lens[[chr]] + 500,
+                      period)),
+        mc.cores=mc.cores
+    )
+    names(cov) <- chroms
+    cov
+}
