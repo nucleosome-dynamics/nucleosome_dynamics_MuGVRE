f10691c504e5ed12949c0738dbe7e9831618414a
rilla <r.illa.pujagut@gmail.com>
2016-04-21 21:33:16 +0200

    path fixed

--- sourced/periodicity_funs.R
+++ sourced/periodicity_funs.R
@@ -5,7 +5,7 @@
 library(IRanges)
 library(parallel)
 
-SOURCE.DIR <- "/home/rilla/nucleServ/rcode/sourced"
+SOURCE.DIR <- "/home/rilla/nucleServ/sourced"
 source(paste(SOURCE.DIR,
              "helperfuns.R",
              sep="/"))
--- tests/nfr.sh
+++ tests/nfr.sh
@@ -2,7 +2,7 @@
 
 ###############################################################################
 
-script="/home/rilla/nucleServ/bin/nucleR.R"
+script="/home/rilla/nucleServ/bin/nfr.R"
 input="/orozco/services/Rdata/tmp_wd/120502_SN365_B_L002_GGM-34_NR.gff"
 output="/orozco/services/Rdata/tmp_wd/120502_SN365_B_L002_GGM-34_nfr.gff"
 
