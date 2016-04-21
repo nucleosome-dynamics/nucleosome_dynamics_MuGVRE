92b1bd4704ea7aafcb43bdd6854efb508fc619a1
rilla <r.illa.pujagut@gmail.com>
2016-04-21 17:29:57 +0200

    script to test coverage.R

---
+++ tests/coverage.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+###############################################################################
+
+script="/home/rilla/nucleServ/bin/coverage.R"
+input="/orozco/services/Rdata/tmp_wd/120502_SN365_B_L002_GGM-34.RData"
+output="/orozco/services/Rdata/tmp_wd/120502_SN365_B_L002_GGM-34_cov.RData"
+
+Rscript $script    \
+    --input $input \
+    --output $output
