3a85dbf9142eb06143cb597fdb947329a77bba5b
rilla <r.illa.pujagut@gmail.com>
2016-04-21 17:21:10 +0200

    script to test readBAM.R

---
+++ tests/readBAM.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+###############################################################################
+
+script="/home/rilla/nucleServ/bin/readBAM.R"
+type="paired"
+input="/orozco/services/Rdata/in_bams/120502_SN365_B_L002_GGM-34.bam"
+output="/orozco/services/Rdata/tmp_wd/120502_SN365_B_L002_GGM-34.RData"
+
+Rscript $script    \
+    --type $type   \
+    --input $input \
+    --output $output
+
+###############################################################################
+
+script="/home/rilla/nucleServ/bin/readBAM.R"
+type="paired"
+input="/orozco/services/Rdata/in_bams/120502_SN365_B_L002_GGM-35.bam"
+output="/orozco/services/Rdata/tmp_wd/120502_SN365_B_L002_GGM-35.RData"
+
+Rscript $script    \
+    --type $type   \
+    --input $input \
+    --output $output
+
+###############################################################################
