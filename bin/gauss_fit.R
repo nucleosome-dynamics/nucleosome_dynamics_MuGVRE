3ed1619bdd2cc495e5ccd6dc83602847574a8ae8
rilla <r.illa.pujagut@gmail.com>
2016-04-22 18:02:40 +0200

    gaussian fitting added

---
+++ bin/gauss_fit.R
@@ -0,0 +1,59 @@
+#!/usr/bin/env Rscript
+
+## Imports ####################################################################
+
+library(getopt)
+library(IRanges)
+library(plyr)
+
+SOURCE.DIR <- "/home/rilla/scratch/nucler/nucleServ/sourced"
+sourced <- c("gff_funs", "fp", "gauss_funs")
+for (x in paste0(SOURCE.DIR, "/", sourced, ".R")) {
+     source(x)
+}
+
+## Command line arguments #####################################################
+
+spec <- matrix(c("calls", "a", 1, "character",
+                 "reads", "b", 1, "character",
+                 "start", "c", 1, "integer",
+                 "end",   "d", 1, "integer",
+                 "chr",   "e", 1, "character"),
+               byrow=TRUE,
+               ncol=4)
+args <- getopt(spec)
+
+## Subset #####################################################################
+
+message("loading inputs")
+calls <- readGff(args[["calls"]])
+reads <- get(load(args[["reads"]]))
+
+if (!is.null(args[["chr"]])) {
+    message("subsetting data")
+
+    calls <- subset(calls, seqname == args[["chr"]])
+    reads <- ranges(reads)[[args[["chr"]]]]
+
+    if (!is.null(args$start) && !is.null(args$end)) {
+        calls <- subset(calls,
+                        isIn(IRanges(start, end),
+                             IRanges(args[["start"]],
+                                     args[["end"]])))
+        reads <- selectReads(reads, calls)
+    }
+}
+
+message("perfroming the fittings")
+gauss.df <- doGaussFit(calls, reads, .progress="text")
+
+## Save output ################################################################
+
+gauss.df$score_w <- NULL
+gauss.df$score_h <- NULL
+gauss.df$nmerge <- NULL
+
+gff <- df2gff(gauss.df)
+writeGff(gff, args[["output"]])
+
+###############################################################################
---
+++ sourced/gauss_funs.R
@@ -0,0 +1,158 @@
+#!/usr/bin/env Rscript
+
+###############################################################################
+
+library(IRanges)
+library(plyr)
+
+###############################################################################
+
+isIn <- function (x, y)
+    # Is x in y?
+    start(x) <= end(y) & end(x) >= start(y)
+
+myFilter <- function (x, f, ...)
+    x[f(x, ...)]
+
+selectReads <- function (reads, range.df)
+    myFilter(reads,
+             isIn,
+             range(with(range.df,
+                        IRanges(start,
+                                end))))
+
+###############################################################################
+
+Gaussian <- function (k=1, m=0, sd=1)
+{
+    runGaussian <- function (x) {
+        num <- (x - m) ** 2
+        denum <- 2 * sd**2
+        k * exp(-num/denum)
+    }
+    plotGaussian <- function (x, ...)
+        plot(x, runGaussian(x), ...)
+    vals <- list(k=k, m=m, sd=sd)
+    toList <- function ()
+        vals
+    structure(c(vals,
+                apply=runGaussian,
+                plot=plotGaussian),
+              class="Gaussian")
+}
+
+print.Gaussian <- function (x)
+{
+    ns <- c("k", "sd", "m")
+    print(paste(mapply(paste,
+                       ns,
+                       lapply(lapply(ns,
+                                     flip2args(`[[`),
+                                     x),
+                              round,
+                              digits=3),
+                       MoreArgs=list(sep="=")),
+                collapse=" "))
+}
+
+as.list.Gaussian <- function (x)
+    list(k=x$k,
+         m=x$m,
+         sd=x$sd)
+
+as.data.frame.Gaussian <- function (x)
+    as.data.frame(as.list(x))
+
+###############################################################################
+
+stripZeros <- function (x, a=0, tail=FALSE)
+{   # remove consecutive zeroes from the beginning of a vector
+    # (or from the end if tail=TRUE)
+    if (tail) {
+        x <- rev(x)
+    }
+    if (x[1] == a) {
+        shift <- rle(x)$lengths[1]
+    } else {
+        shift <- 0
+    }
+    x <- x[-(1:shift)]
+    if (tail) {
+        x <- rev(x)
+    }
+    list(x=x, shift=shift)
+}
+
+fitIt <- function (f, tries=10, max.value=35000, max.sd=2000)
+{
+    isFitOk <- function (fit)
+        fit$value <= max.value && fit$par[2] <= max.sd
+    for (i in 1:tries) {
+        par <- 10 * runif(3)
+        fit <- optim(par,
+                     f,
+                     method="BFGS",
+                     control=list(reltol=1e-9))
+        ok <- isFitOk(fit)
+        if (ok) {
+            break
+        }
+    }
+    if (ok) {
+        return(fit$par)
+    } else {
+        return(NULL)
+    }
+}
+
+gaussFitCov <- function (xs)
+{
+    xs <- as.vector(xs)
+    a <- stripZeros(xs, 0)
+    xs <- a$x
+    shift <- a$shift
+    xs <- stripZeros(xs, 0, tail=TRUE)$x
+    f <- function (par) {
+        m <- par[1]
+        sd <- par[2]
+        k <- par[3]
+        rhat <- k * exp(-0.5 * ((seq_along(xs) - m)/sd)^2)
+        sum((xs - rhat)^2)
+    }
+    fit.par <- fitIt(f)
+    if (is.null(fit.par)) {
+        return(NULL)
+    } else {
+        return(do.call(Gaussian,
+                       list(m=fit.par[[1]] + shift,
+                            sd=fit.par[[2]],
+                            k=fit.par[[3]])))
+    }
+}
+
+###############################################################################
+
+gaussFitChr <- function (calls, reads, ...)
+    adply(calls,
+          1,
+          function (x)
+              as.data.frame(gaussFitCov(coverage(selectReads(reads, x)))),
+          ...)
+
+doGaussFit <- function (calls, reads, ...)
+{
+    if (class(reads) == "RangedData") {
+        rs <- ranges(reads)
+        doChr <- function (chr.calls) {
+            chr <- chr.calls[1, "seqname"]
+            message(chr)
+            chr.reads <- rs[[chr]]
+            gaussFitChr(chr.calls, chr.reads, ...)
+        }
+        ddply(calls, "seqname", doChrm)
+    } else if (class(reads) == "IRanges") {
+        gaussFitChr(calls, reads, ...)
+    }
+}
+
+###############################################################################
