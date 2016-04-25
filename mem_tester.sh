c72efb3282561e1592b3ec4abd56324a5c0cd4a3
rilla <r.illa.pujagut@gmail.com>
2016-04-25 14:51:33 +0200

    .

--- mem_tester.sh
+++ mem_tester.sh
@@ -7,15 +7,12 @@ scriptdir="/home/rilla/nucleServ/tests"
 #> $logf
 > $proclog
 
-#for f in $scriptdir/*.sh
-#do
-#    calcname=$(basename $f ".sh")
-#    echo $calcname >> $logf
-#    echo $calcname > $proclog
-#    echo $calcname
-#    /usr/bin/time -avo $logf $f 2> $proclog
-#    echo "========" >> $logf
-#done
-
-f=/home/rilla/nucleServ/tests/nucleosomeDynamics.sh
-/usr/bin/time -avo $logf $f 2> $proclog
+for f in $scriptdir/*.sh
+do
+    calcname=$(basename $f ".sh")
+    echo $calcname >> $logf
+    echo $calcname > $proclog
+    echo $calcname
+    /usr/bin/time -avo $logf $f 2> $proclog
+    echo "========" >> $logf
+done
